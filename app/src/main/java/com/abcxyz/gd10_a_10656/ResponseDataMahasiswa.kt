d9125b68fff09cabfb384762b4c06bc9596071a1
rinlabs <200710656@students.uajy.ac.id>
2022-11-08 15:28:03 +0700

    Initial commit

---
+++ app/src/main/java/com/abcxyz/gd10_a_10656/ResponseDataMahasiswa.kt
@@ -0,0 +1,8 @@
+package com.abcxyz.gd10_a_10656
+
+import com.google.gson.annotations.SerializedName
+
+data class ResponseDataMahasiswa(
+    @SerializedName("status") val stt:String,
+    val totaldata: Int,
+    val data:List<MahasiswaData>)
